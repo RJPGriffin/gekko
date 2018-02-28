23c942e07c7d27bf8c05cad62682aba72b6c220f
Rowan Griffin <rowangee@gmail.com>
2018-02-28 15:41:21 +0000

    Fixed Brackets

--- strategies/ATR_ADX.js
+++ strategies/ATR_ADX.js
@@ -13,155 +13,156 @@ var config = require('../core/util.js').getConfig();
 // strategy
 var strat = {
 
-    /* INIT */
-    init: function() {
-      // core
-      this.name = 'Adaptive ATR ADX';
-      this.requiredHistory = config.tradingAdvisor.historySize;
+  /* INIT */
+  init: function() {
+    // core
+    this.name = 'Adaptive ATR ADX';
+    this.requiredHistory = config.tradingAdvisor.historySize;
 
-      // debug? set to false to disable all logging/messages/stats (improves performance in backtests)
-      this.debug = false;
+    // debug? set to false to disable all logging/messages/stats (improves performance in backtests)
+    this.debug = false;
 
-      // performance
-      config.backtest.batchSize = 1000; // increase performance
-      config.silent = true;
-      config.debug = false;
+    // performance
+    config.backtest.batchSize = 1000; // increase performance
+    config.silent = true;
+    config.debug = false;
 
-      // ATR
-      this.addTulipIndicator('ATR', 'atr', {
-        optInTimePeriod: this.settings.ATR
-      });
+    // ATR
+    this.addTulipIndicator('ATR', 'atr', {
+      optInTimePeriod: this.settings.ATR
+    });
 
-      // ADX
-      this.addTulipIndicator('ADX', 'adx', {
-        optInTimePeriod: this.settings.ADX
-      })
+    // ADX
+    this.addTulipIndicator('ADX', 'adx', {
+      optInTimePeriod: this.settings.ADX
+    })
 
-      //Directional Indicator - Needed to decide initial long or short position on strat start
-      this.addTulipIndicator('DI', 'di', {
-        optInTimePeriod: this.settings.ADX //matches the ADX period
-      })
+    //Directional Indicator - Needed to decide initial long or short position on strat start
+    this.addTulipIndicator('DI', 'di', {
+      optInTimePeriod: this.settings.ADX //matches the ADX period
+    })
 
-      //High and low
-      this.highs = new Array(this.settings.ATR);
-      this.lows = new Array(this.settings.ATR);
-      this.arrayCounter = 0; // to track array position
-      this.periodMax = 0;
-      this.periodMin = 0;
+    //High and low
+    this.highs = new Array(this.settings.ATR);
+    this.lows = new Array(this.settings.ATR);
+    this.arrayCounter = 0; // to track array position
+    this.periodMax = 0;
+    this.periodMin = 0;
 
-      //Variables
-      this.trend = 'none';
-      this.newTrend = false;
-      this.stop;
-
-
-
-      //ATR Level Modifiers
-      this.ATR_threshold = this.settings.ATR_threshold;
-      this.ATR_Multiplier_high = this.settings.ATR_Multiplier_high;
-      this.ATR_Multiplier_low = this.settings.ATR_Multiplier_low;
+    //Variables
+    this.trend = 'none';
+    this.newTrend = false;
+    this.stop;
 
 
-      // debug stuff
-      this.startTime = new Date();
+
+    //ATR Level Modifiers
+    this.ATR_threshold = this.settings.ATR_threshold;
+    this.ATR_Multiplier_high = this.settings.ATR_Multiplier_high;
+    this.ATR_Multiplier_low = this.settings.ATR_Multiplier_low;
 
 
-      /* MESSAGES */
+    // debug stuff
+    this.startTime = new Date();
 
-      // message the user about required history
-      log.info("====================================");
-      log.info('Running', this.name);
-      log.info('====================================');
-      log.info("Make sure your warmup period matches the longer of ATR or ADX and that Gekko downloads data if needed");
 
-      // warn users
-      if (this.requiredHistory < this.settings.ATR || this.requiredHistory < this.settings.ATR) {
-        log.warn("*** WARNING *** Your Warmup period is lower than ATR|ADX. If Gekko does not download data automatically when running LIVE the strategy will not behave as expected");
+    /* MESSAGES */
+
+    // message the user about required history
+    log.info("====================================");
+    log.info('Running', this.name);
+    log.info('====================================');
+    log.info("Make sure your warmup period matches the longer of ATR or ADX and that Gekko downloads data if needed");
+
+    // warn users
+    if (this.requiredHistory < this.settings.ATR || this.requiredHistory < this.settings.ATR) {
+      log.warn("*** WARNING *** Your Warmup period is lower than ATR|ADX. If Gekko does not download data automatically when running LIVE the strategy will not behave as expected");
+    }
+
+  }, // init()
+
+  updateMinMax: function(candle) {
+    // Add latest high and low to arrays at counter index
+    this.lows[this.arrayCounter] = candle.low;
+    this.highs[this.arrayCounter] = candle.high;
+    //increment index
+    this.arrayCounter = this.arrayCounter + 1;
+
+    if (this.arrayCounter === this.settings.ATR) { //reset index if at array length
+      this.arrayCounter = 0;
+    }
+
+    //Set newest period max and min
+    this.periodMax = Math.max.apply(Math, this.highs);
+    this.periodMin = Math.min.apply(Math, this.lows);
+
+  },
+
+  getBullMultiplier: function(adx) {
+    if (adx < this.ATR_threshold) {
+      return this.BULL_Multiplier_high;
+    } else {
+      return this.BULL_Multiplier_low;
+    }
+  },
+
+  getBearMultiplier: function(adx) {
+    if (adx < this.ATR_threshold) {
+      return this.BEAR_Multiplier_high;
+    } else {
+      return this.BEAR_Multiplier_low;
+    }
+  },
+
+  update: function(candle) {
+    this.updateMinMax(candle);
+  },
+
+
+  /* CHECK */
+  check: function(candle) {
+    // get all indicators
+    let ind = this.tulipIndicators,
+      atr = ind.ATR.result.result,
+      adx = ind.ADX.result.result,
+      di = ind.DI.result,
+      price = candle.close;
+
+
+
+    //Check for long
+    if (this.trend === 'bull') {
+      //Calculate new stop target
+      //Bull trend so stop needs to be below the trendline.
+
+      let newStop = this.periodMax - (atr * this.getBullMultiplier(adx))
+
+      if (newStop > this.stop || this.newTrend) {
+        this.stop = newStop;
+        this.newTrend = false;
       }
 
-    }, // init()
-
-    updateMinMax: function(candle) {
-      // Add latest high and low to arrays at counter index
-      this.lows[this.arrayCounter] = candle.low;
-      this.highs[this.arrayCounter] = candle.high;
-      //increment index
-      this.arrayCounter = this.arrayCounter + 1;
-
-      if (this.arrayCounter === this.settings.ATR) { //reset index if at array length
-        this.arrayCounter = 0;
+      //check if price has hit target
+      if (candle.close <= this.stop) {
+        this.short();
       }
 
-      //Set newest period max and min
-      this.periodMax = Math.max.apply(Math, this.highs);
-      this.periodMin = Math.min.apply(Math, this.lows);
 
-    },
+    } else if (this.trend === 'bear') {
+      //Calculate new stop target
+      let newStop = this.periodMin + (atr * this.getBearMultiplier(adx))
 
-    getBullMultiplier: function(adx) {
-      if (adx < this.ATR_threshold) {
-        return this.BULL_Multiplier_high;
-      } else {
-        return this.BULL_Multiplier_low;
-      }
-    },
-
-    getBearMultiplier: function(adx) {
-      if (adx < this.ATR_threshold) {
-        return this.BEAR_Multiplier_high;
-      } else {
-        return this.BEAR_Multiplier_low;
-      }
-    },
-
-    update: function(candle) {
-      this.updateMinMax(candle);
-    },
-
-
-    /* CHECK */
-    check: function(candle) {
-      // get all indicators
-      let ind = this.tulipIndicators,
-        atr = ind.ATR.result.result,
-        adx = ind.ADX.result.result,
-        di = ind.DI.result,
-        price = candle.close;
-
-
-
-      //Check for long
-      if (this.trend === 'bull') {
-        //Calculate new stop target
-        //Bull trend so stop needs to be below the trendline.
-
-        let newStop = this.periodMax - (atr * this.getBullMultiplier(adx))
-
-        if (newStop > this.stop || this.newTrend) {
-          this.stop = newStop;
-          this.newTrend = false;
-        }
-
-        //check if price has hit target
-        if (candle.close <= this.stop) {
-          this.short();
-        }
-
-
-      } else if (this.trend === 'bear') {
-        //Calculate new stop target
-        let newStop = this.periodMin + (atr * this.getBearMultiplier(adx))
-
-        if (newStop < this.stop || this.newTrend) {
-          this.stop = newStop;
-          this.newTrend = false;
-        }
+      if (newStop < this.stop || this.newTrend) {
+        this.stop = newStop;
+        this.newTrend = false;
       }
       //check if price has hit target
       if (candle.close >= this.stop) {
         this.long();
       }
     }
+
+
     //check if first run - make long or short decision
     else if (this.trend === 'none') {
       if (di.plus_di > di.minus_di) { //BULL
