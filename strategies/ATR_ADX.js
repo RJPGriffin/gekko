5019ea9b072a6dd703d1f6ed7d9109aa0c06054e
Rowan Griffin <rowangee@gmail.com>
2018-03-01 10:24:24 +0000

    Updated ATR_ADX to Heiken Ashi candles, and other tweaks

--- strategies/ATR_ADX.js
+++ strategies/ATR_ADX.js
@@ -9,6 +9,8 @@
 // req's
 var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
+var _ = require('lodash');
+
 
 // strategy
 var strat = {
@@ -26,6 +28,15 @@ var strat = {
     config.backtest.batchSize = 1000; // increase performance
     config.silent = true;
     config.debug = false;
+
+    // Heiken Ashi candle
+    this.HACandle;
+    this.previousCandle = { //Initialise previousCandle with 0
+      "open": 0,
+      "close": 0,
+      "high": 0,
+      "low": 0
+    };
 
     // ATR
     this.addTulipIndicator('ATR', 'atr', {
@@ -58,8 +69,8 @@ var strat = {
 
     //ATR Level Modifiers
     this.ATR_threshold = this.settings.ATR_threshold;
-    this.ATR_Multiplier_high = this.settings.ATR_Multiplier_high;
-    this.ATR_Multiplier_low = this.settings.ATR_Multiplier_low;
+    // this.ATR_Multiplier_high = this.settings.ATR_Multiplier_high;
+    // this.ATR_Multiplier_low = this.settings.ATR_Multiplier_low;
 
 
     // debug stuff
@@ -81,7 +92,18 @@ var strat = {
 
   }, // init()
 
+  heikenAshi: function(candle) {
+    return {
+      close: (candle.open + candle.close + candle.high + candle.low) / 4,
+      open: (this.previousCandle.open + this.previousCandle.close) / 2,
+      high: _.max([candle.high, candle.open, candle.close]),
+      low: _.min([candle.low, candle.open, candle.close])
+    };
+  },
+
   updateMinMax: function(candle) {
+    //Gets the minimum and maximum trade price from the last ATR Period
+
     // Add latest high and low to arrays at counter index
     this.lows[this.arrayCounter] = candle.low;
     this.highs[this.arrayCounter] = candle.high;
@@ -98,52 +120,99 @@ var strat = {
 
   },
 
+  updateMinMaxAverage: function(candle) {
+    //same as above except it outputs the average low/high instead of the min/max
+
+    // Add latest high and low to arrays at counter index
+    this.lows[this.arrayCounter] = candle.low;
+    this.highs[this.arrayCounter] = candle.high;
+    //increment index
+    this.arrayCounter = this.arrayCounter + 1;
+
+    if (this.arrayCounter === this.settings.ATR) { //reset index if at array length
+      this.arrayCounter = 0;
+    }
+
+    let averageMin = 0,
+      averageMax = 0;
+
+    for (let i in this.lows) {
+      averageMin += this.lows[i];
+      averageMax += this.highs[i];
+    }
+
+    //Set newest period max and min
+    this.periodMax = averageMin / this.lows.length;
+    this.periodMin = averageMax / this.highs.length;
+
+  },
+
+  resetMinMax: function() {
+    // Resets the minimum and maximum arrays.
+    // Thought is that doing this each trade limits the effect of past data that is no longer that relevant
+    for (var i in this.lows) {
+      this.lows[i] = this.HACandle.low;
+      this.highs[i] = this.HACandle.high;
+    }
+  },
+
   getBullMultiplier: function(adx) {
+    // Returns the correct Bull multiplier depending on ADX value
     if (adx < this.ATR_threshold) {
-      return this.BULL_Multiplier_high;
+      return this.settings.BULL_Multiplier_high;
     } else {
-      return this.BULL_Multiplier_low;
+      return this.settings.BULL_Multiplier_low;
     }
   },
 
   getBearMultiplier: function(adx) {
+    // Returns the correct Bear multiplier depending on ADX value
     if (adx < this.ATR_threshold) {
-      return this.BEAR_Multiplier_high;
+      return this.settings.BEAR_Multiplier_high;
     } else {
-      return this.BEAR_Multiplier_low;
+      return this.settings.BEAR_Multiplier_low;
     }
   },
 
+  //called on each new candle, before check.
   update: function(candle) {
-    this.updateMinMax(candle);
+    // this.updateMinMax(candle);
+    this.HACandle = this.heikenAshi(candle);
+    this.updateMinMaxAverage(this.HACandle);
+    this.previousCandle = candle; //for HA calculation
   },
 
 
   /* CHECK */
-  check: function(candle) {
+  check: function() {
     // get all indicators
     let ind = this.tulipIndicators,
       atr = ind.ATR.result.result,
       adx = ind.ADX.result.result,
       di = ind.DI.result,
-      price = candle.close;
+      price = this.HACandle.close;
 
-
+    //Reset the min max if the trend is new
+    if (this.newTrend) {
+      this.resetMinMax(this.HACandle);
+    }
 
     //Check for long
     if (this.trend === 'bull') {
       //Calculate new stop target
-      //Bull trend so stop needs to be below the trendline.
 
+      //Bull trend so stop needs to be below the trendline.
       let newStop = this.periodMax - (atr * this.getBullMultiplier(adx))
 
+      // Stop can only increase, therefore only use the new stop if it is higher than current stop
+      // If trend is new, bull stop will never be higher than bear stop, therefore use it anyway.
       if (newStop > this.stop || this.newTrend) {
         this.stop = newStop;
         this.newTrend = false;
       }
 
-      //check if price has hit target
-      if (candle.close <= this.stop) {
+      //If candle close price has passed the latest stop, change advice to short
+      if (price <= this.stop) {
         this.short();
       }
 
@@ -157,13 +226,13 @@ var strat = {
         this.newTrend = false;
       }
       //check if price has hit target
-      if (candle.close >= this.stop) {
+      if (price >= this.stop) {
         this.long();
       }
     }
 
 
-    //check if first run - make long or short decision
+    // This will only run on the very first candle. Buys if current trend is bull, sells if bear.
     else if (this.trend === 'none') {
       if (di.plus_di > di.minus_di) { //BULL
         this.long();
@@ -181,10 +250,8 @@ var strat = {
       log.info('\n\n');
     }
 
+
   }, // check()
-
-
-
 
   /* LONG */
   long: function() {
@@ -195,7 +262,6 @@ var strat = {
       this.advice('long');
     }
   },
-
 
   /* SHORT */
   short: function() {
