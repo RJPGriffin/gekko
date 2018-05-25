e60d4d451a7a522e638f65042af5e81dd8f1d281
Rowan Griffin <rowangee@gmail.com>
2018-05-25 15:32:10 +0100

    Gforms v2 work

--- plugins/gforms.js
+++ plugins/gforms.js
@@ -12,13 +12,31 @@ var gfc = config.gforms;
 
 var gforms = function(done) {
   _.bindAll(this);
-  log.debug('gforms function');
 
-  this.formID = gfc.formID;
-  this.formUrl = 'https://docs.google.com/forms/d/e/' + this.formID + '/formResponse?usp=pp_url&'
 
   //Track advice, linked to trades
   this.advicePrice = 0;
+  this.adviceTime = 0;
+  this.question = [];
+
+  var prefill = gfc.prefill;
+  prefill = prefill.slice(34, prefill.length);
+  var result = prefill.search('/');
+  this.formID = prefill.slice(0, result);
+  prefill = prefill.slice(result + 20, prefill.length);
+  this.formUrl = 'https://docs.google.com/forms/d/e/' + this.formID + '/formResponse?usp=pp_url&'
+
+  let count = 0;
+
+  while (count < 11) {
+    var start = prefill.search('entry') + 6;
+    var end = prefill.search('=');
+    this.question.push(prefill.slice(start, end));
+    prefill = prefill.slice(end + 2, prefill.length);
+    count++
+  }
+
+  //TODO - Add check that above has actually worked.
 
   this.done = done;
   this.setup();
@@ -34,38 +52,52 @@ gforms.prototype.setup = function(done) {
 };
 
 gforms.prototype.processAdvice = function(advice) {
-  //Get advice price for pair
+  //Get advice price and time
   this.advicePrice = advice.candle.close;
+  this.adviceTime = Date.now();
 };
-
-
-// &entry.1346916648=exchange&entry.1743858251=currency&entry.105864059=Asset&entry.68010386=Event&
-//entry.1463011579=Price&entry.1529244935=Date&entry.3616735=AssetInPorf&entry.433943481=CurrenInPort&
-//entry.1202282384=PortBalance&entry.620326103=Balance
 
 gforms.prototype.processTrade = function(trade) {
   let currency = config.watch.currency;
   let asset = config.watch.asset;
   let exchange = config.watch.exchange;
+  let tradeTime = Date.now();
 
-  //build up string
+  let timeToComplete = (tradeTime - this.adviceTime) * 60000; //Difference in ms, converted to minutes
+
+  //build up data string
   let dataString =
-    'entry.' + gfc.exchange + '=' + exchange + '&' +
-    'entry.' + gfc.currency + '=' + currency + '&' +
-    'entry.' + gfc.asset + '=' + asset + '&' +
-    'entry.' + gfc.action + '=' + trade.action + '&' +
-    'entry.' + gfc.price + '=' + trade.price + '&' +
-    'entry.' + gfc.assetCount + '=' + trade.portfolio.asset + '&' +
-    'entry.' + gfc.currencyCount + '=' + trade.portfolio.currency + '&' +
-    'entry.' + gfc.advicePrice + '=' + this.advicePrice + '&' +
-    'entry.' + gfc.balance + '=' + trade.balance;
+    'entry.' + this.question[0] + '=' + gfc.botTag + '&' +
+    'entry.' + this.question[1] + '=' + exchange + '&' +
+    'entry.' + this.question[2] + '=' + currency + '&' +
+    'entry.' + this.question[3] + '=' + asset + '&' +
+    'entry.' + this.question[4] + '=' + trade.action + '&' +
+    'entry.' + this.question[5] + '=' + trade.portfolio.asset + '&' +
+    'entry.' + this.question[6] + '=' + trade.price + '&' +
+    'entry.' + this.question[7] + '=' + trade.portfolio.currency + '&' +
+    'entry.' + this.question[8] + '=' + trade.balance + '&' +
+    'entry.' + this.question[9] + '=' + this.advicePrice + '&' +
+    'entry.' + this.question[10] + '=' + timeToComplete;
 
-  //log.info(this.formUrl + dataString);
+  /*
+  Index: (-1 for array index
+  1: Tag
+  2: Exchange
+  3: Currency
+  4: Asset
+  5: Action
+  6: Asset in Portfolio
+  7: Price
+  8: Currency in Portfolio
+  9: Balance
+  10: Advice Price
+  11: Time to Fill
+*/
 
-  request.post(this.formUrl + dataString + '&submit=Submit', function(error, response) {
-    //console.log('error:', error); // Print the error if one occurred
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  });
+  log.info("Sending Trade Data to your Google Sheet");
+  log.info(this.formUrl + dataString);
+
+  request.post(this.formUrl + dataString + '&submit=Submit', function(error, response) {});
 
 
 };
